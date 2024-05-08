8c6ca2557fc7fca7ec0a18c074fee86c79026508
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2024-05-08 15:35:43 +0200

    Updated classes

--- raytracing.h
+++ raytracing.h
@@ -20,10 +20,36 @@ public:
     float y;
     float z;
 
-    // Find distance between current and passed points.
-    virtual float distance(Primitive* point)
+    Primitive()
     {
-        return std::sqrt(x * point->x + y * point->y + z * point->z);
+        x = y = z = 0;
+    }
+    Primitive(float x1, float y1, float z1)
+    {
+        x = x1;
+        y = y1;
+        z = z1;
+    }
+};
+
+// Class that represents camera.
+class Camera : public Primitive
+{
+public:
+    int width;
+    int height;
+
+    Camera()
+    {
+        width = height = 0;
+    }
+    Camera(float x1, float y1, float z1, int screenWidth, int screenHeight)
+    {
+        x = x1;
+        y = y1;
+        z = z1;
+        width = screenWidth;
+        height = screenHeight;
     }
 };
 
@@ -31,6 +57,8 @@ public:
 class Vector : public Primitive
 {
 public:
+    using Primitive::Primitive;
+
     // Calculate vector length.
     virtual float length()
     {
@@ -42,10 +70,37 @@ public:
     {
         return x * vector->x + y * vector->y + z * vector->z;
     }
-private:
-    float distance(Primitive* point) override
+};
+
+// Base class that represents 3D object.
+class Object : public Primitive
+{
+public:
+    // Material parameters.
+    COLORREF color;
+
+    Object()
     {
-        return length();
+        x = y = z = color = 0;
+    }
+    Object(float x1, float y1, float z1, COLORREF objectColor)
+    {
+        x = x1;
+        y = y1;
+        z = z1;
+        color = objectColor;
+    }
+
+    virtual float intersect(Vector* vector)
+    {
+        // Calculate relation of Object's x and vector's x.
+        float t = x / vector->x;
+
+        // Check if relation is the same for every coordinate.
+        // If so, vector intersects with object.
+        if (t == y / vector->y && t == z / vector->z) return t;
+
+        return -1;
     }
 };
 
@@ -56,6 +111,19 @@ public:
     // Light parameters.
     COLORREF color;
     float power;
+
+    Light()
+    {
+        x = y = z = color = power = 0;
+    }
+    Light(float x1, float y1, float z1, COLORREF lightColor, float lightPower)
+    {
+        x = x1;
+        y = y1;
+        z = z1;
+        color = lightColor;
+        power = lightPower;
+    }
 
     // Calculate exposure of object's point to light.
     float countLight(Primitive* objectPoint, Primitive* objectCenter)
@@ -83,7 +151,7 @@ public:
     }
 
     // Calculate color of object in light.
-    COLORREF lightColor(Object* object)
+    COLORREF lightColor(Object* object, float coefficient)
     {
         // COLORREF (typedef DWORD) format: 0x00BBGGRR
         COLORREF newObjectColor = 0;
@@ -95,7 +163,7 @@ public:
             char objectChannel = (object->color >> i) % 255;
 
             // Create new color channel.
-            char newObjectChannel = std::round((lightChannel + objectChannel) * power);
+            char newObjectChannel = std::round((lightChannel + objectChannel) * power * coefficient);
 
             if (newObjectChannel > 0xFF) newObjectChannel = 0xFF;
 
@@ -107,32 +175,25 @@ public:
     }
 };
 
-// Base class that represents 3D object.
-class Object : public Primitive
-{
-public:
-    // Material parameters.
-    COLORREF color;
-
-    virtual float intersect(Vector* vector)
-    {
-        // Calculate relation of Object's x and vector's x.
-        float t = x / vector->x;
-
-        // Check if relation is the same for every coordinate.
-        // If so, vector intersects with object.
-        if (t == y / vector->y && t == z / vector->z) return t;
-
-        return -1;
-    }
-};
-
 // Class to store sphere info.
 class Sphere : public Object
 {
 public:
     // Sphere parameters.
     float radius;
+
+    Sphere()
+    {
+        x = y = z = color = radius = 0;
+    }
+    Sphere(float x1, float y1, float z1, COLORREF sphereColor, float sphereRadius)
+    {
+        x = x1;
+        y = y1;
+        z = z1;
+        color = sphereColor;
+        radius = sphereRadius;
+    }
 
     float intersect(Vector* vector) override
     {
@@ -157,10 +218,60 @@ public:
     }
 };
 
+// Class that contains info about whole scene (light sources and objects).
+class Scene
+{
+public:
+    Scene()
+    {
+        camera = NULL;
+    }
+    Scene(Camera* sceneCamera)
+    {
+        camera = sceneCamera;
+    }
+
+    void setCamera(Camera* newCamera)
+    {
+        // Free memory of current camera.
+        if (camera) delete camera;
+
+        camera = newCamera;
+    }
+
+    void addLight(Light* light)
+    {
+        lightSources.push_back(light);
+    }
+
+    void addObject(Object* object)
+    {
+        objects.push_back(object);
+    }
+
+    // Free all memory.
+    void clear()
+    {
+        for (Light* l : lightSources)
+        {
+            if (l) delete l;
+        }
+
+        for (Object* o : objects)
+        {
+            if (o) delete o;
+        }
+    }
+
+private:
+    Camera* camera;
+    std::vector<Light*> lightSources;
+    std::vector<Object*> objects;
+};
+
 // Function prototypes.
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-int drawScene(HWND hwnd);
-std::vector<std::vector<Primitive*>*>* createScene();
-void deleteScene(std::vector<std::vector<Primitive*>*>* scene);
+Scene createScene(int screenWidth, int screenHeight);
+int renderScene(HWND hwnd, Scene* scene);
 void showError(const std::wstring& wstrError);
 void showValue(const std::wstring& wstrName, auto aValue);
